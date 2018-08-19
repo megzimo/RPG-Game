b9afbf39b66b53c72955d483545cd0aa0411c25f
megzimo <40956115+megzimo@users.noreply.github.com>
2018-08-19 16:45:45 -0500

    images added

---
+++ assets/javascript/rpg.js
@@ -0,0 +1,30 @@
+let characters = {
+    gregg: {
+        name: "Old Gregg",
+        health: 200,
+        attackValue: 25, //how much damage the character can do to other characters 
+        damageValue: 15, //how much damage the character receives when attacked
+        img: "../images/old-gregg"
+    },
+    bollo: {
+        name: "Old Gregg",
+        health: 180,
+        attackValue: 25,
+        damageValue: 15,
+        img: "../images/bollo"
+    },
+    howard: {
+        name: "Old Gregg",
+        health: 130,
+        attackValue: 10,
+        damageValue: 25,
+        img: "../images/howard-moon"
+    },
+    naboo: {
+        name: "Old Gregg",
+        health: 190,
+        attackValue: 25,
+        damageValue: 15,
+        img: "../images/naboo"
+    },
+}
