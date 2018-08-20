a41168efcedfb851d1443c4ed283a1b27435b406
megzimo <40956115+megzimo@users.noreply.github.com>
2018-08-20 12:11:22 -0500

    images and combat zone added

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -6,28 +6,24 @@ $(document).ready(function () {
             health: 200,
             attackValue: 25, //how much damage the character can do to other characters 
             damageValue: 15, //how much damage the character receives when attacked
-            img: "../images/old-gregg"
         },
         bollo: {
             name: "Old Gregg",
             health: 180,
             attackValue: 25,
             damageValue: 15,
-            img: "../images/bollo"
         },
         howard: {
             name: "Old Gregg",
             health: 130,
             attackValue: 10,
             damageValue: 25,
-            img: "../images/howard-moon"
         },
         naboo: {
             name: "Old Gregg",
             health: 190,
             attackValue: 25,
             damageValue: 15,
-            img: "../images/naboo"
         },
     }
 
