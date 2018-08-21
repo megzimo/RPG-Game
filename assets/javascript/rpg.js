a3fabe3fba5b3dec61e79218998403481c9f54b1
megzimo <40956115+megzimo@users.noreply.github.com>
2018-08-21 16:28:27 -0500

    template literals added

--- assets/javascript/rpg.js
+++ assets/javascript/rpg.js
@@ -1,34 +1,65 @@
 $(document).ready(function () {
 
-    let characters = {
-        gregg: {
+    let characters = [
+         {
             name: "Old Gregg",
             health: 200,
             attackValue: 25, //how much damage the character can do to other characters 
             damageValue: 15, //how much damage the character receives when attacked
+            img: "assets/images/old-gregg.jpg",
         },
-        bollo: {
-            name: "Old Gregg",
+         {
+            name: "Bollo",
             health: 180,
             attackValue: 25,
             damageValue: 15,
+            img: "assets/images/bollo.jpg",
         },
-        howard: {
-            name: "Old Gregg",
+         {
+            name: "Howard Moon",
             health: 130,
             attackValue: 10,
             damageValue: 25,
+            img: "assets/images/howard-moon.jpg",
         },
-        naboo: {
-            name: "Old Gregg",
+         {
+            name: "Naboo",
             health: 190,
             attackValue: 25,
             damageValue: 15,
+            img: "assets/images/naboo.jpg",
         }
-    };
+    ];
 
-//Moving characters from group to combat area//
-$('.arena').on('click', function() {
-    gregg.appendTo(".arena");
+let charChoice; // character chosen by player
+let opponents = []; // all available opponents queued
+let defender; // current opponent to battle
+let attack = 10; // baseline amount of damage player can do
+let damage; // damage done to player by opponents
+let victory = ""; // message to be displayed upon win
+let defeat = ""; // message to be displayed upon loss
+
+// creating resets to ensure DOM and selections are clear
+characters.map(character => { 
+    console.log(character.name)
+    console.log(character.health) 
+
+    let charCard = 
+        `<div class="card border-secondary mb-3" style="max-width: 18rem;" id="character"> 
+            <div class="card-header bg-transparent border-secondary">${character.name}</div>
+                <div class="card-body text-success">
+                    <img src=${character.img} class="icon">   
+                </div>
+            <div class="card-footer bg-transparent border-secondary">Health: ${character.health}</div>
+        </div>`
+   
+   
+   
+    $(charCard).appendTo('.card-group').html; //
+ 
 })
-})
+
+
+
+
+}) // ENDS .ready(function)
